9f0721fdfcd6c7369823c40f75f9273553fe88c1
Max <maxibonino@users.noreply.github.com>
2020-08-19 23:39:55 +0400

    Added location-review-form view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,5 +10,5 @@ module.exports.locationInfo = function(req, res, next) {
 
 // Страница отзыва
 module.exports.addReview = function(req, res, next) {
-  res.render('index', { title: 'Add review' });
+  res.render('location-review-form', { title: 'Add review' });
 }
