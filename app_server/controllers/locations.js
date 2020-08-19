76c74e4910446a77064c17eb4acbda40ffa62c33
Max <maxibonino@users.noreply.github.com>
2020-08-19 19:54:28 +0400

    Location-info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 /* Получить (GET) домашнюю страницу */
 module.exports.homelist = function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('locations-list', { title: 'Home' });
 };
 
 // Информация о местоположении
 module.exports.locationInfo = function(req, res, next) {
-  res.render('index', { title: 'Location Info' });
+  res.render('location-info', { title: 'Location Info' });
 }
 
 // Страница отзыва
